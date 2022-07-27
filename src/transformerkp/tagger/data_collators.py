57a21297bbd3715ee8ef44ef4ca2238336e454dd
Amardeep <kumaramardipsingh@gmail.com>
2022-07-27 20:07:33 +0530

    add data collator

--- src/transformerkp/tagger/data_collators.py
+++ src/transformerkp/tagger/data_collators.py
@@ -2,4 +2,8 @@ from transformers import DataCollatorForTokenClassification
 
 
 class DataCollatorForKpExtraction(DataCollatorForTokenClassification):
+    """
+    A Data collator class for keyphrase extraction built on the top of HF DataCollatorForTokenClassification collator.
+    """
+
     pass
