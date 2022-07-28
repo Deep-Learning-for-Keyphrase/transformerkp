e4afe23863e6c7984ac562d6ea822a432334e9a1
Amardeep <kumaramardipsingh@gmail.com>
2022-07-28 22:41:15 +0530

    add args

---
+++ src/transformerkp/tagger/args.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass, field
+from typing import Callable, Optional
+
+from transformers import TrainingArguments
+
+
+@dataclass
+class KETrainingArguments(TrainingArguments):
+    """
+    A custom training argument class for keyphrase extraction, extending HF's TrainingArguments class.
+    """
+
+    return_keyphrase_level_metrics: bool = field(
+        default=True,
+        metadata={
+            "help": "Whether to return keyphrase level metrics during evaluation or just the BIO tag level."
+        },
+    )
+
+    score_aggregation_method: bool = field(
+        default="avg",
+        metadata={
+            "help": "which method among avg, max and first to use while calculating confidence score of a keyphrase. None indicates not to calculate this s ore"
+        },
+    )
