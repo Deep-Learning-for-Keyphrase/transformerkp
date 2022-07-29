08ac5546e67f5f91e5ba31b76c002b500f4d30cb
Amardeep <kumaramardipsingh@gmail.com>
2022-07-30 02:07:21 +0530

    add automodels

---
+++ src/transformerkp/tagger/models/auto_models.py
@@ -0,0 +1,55 @@
+from transformers import (
+    AutoModelForTokenClassification,
+    PretrainedConfig,
+    AutoConfig,
+)
+from .models import BertCrfModelForKpExtraction, RobertaCrfForKpExtraction
+
+CRF_MODEL_MAPPINGS = {
+    "BertConfig": BertCrfModelForKpExtraction,
+    "RobertaConfig": RobertaCrfForKpExtraction,
+}
+
+
+class AutoModelForKPExtraction(AutoModelForTokenClassification):
+    pass
+
+
+class AutoCrfModelForKPExtraction:
+    def __init__(self, *args, **kwargs) -> None:
+        raise EnvironmentError(
+            f"{self.__class__.__name__} is designed to be instantiated "
+            f"using the `{self.__class__.__name__}.from_pretrained(model_name_or_path)` or "
+            f"`{self.__class__.__name__}.from_config(config)` methods."
+        )
+
+    @classmethod
+    def from_config(cls, config, **kwargs):
+        if type(config) in CRF_MODEL_MAPPINGS.keys():
+            model_class = CRF_MODEL_MAPPINGS[type(config)]
+            return model_class._from_config(config, **kwargs)
+
+        raise ValueError(
+            f"Unrecognized configuration class {config.__class__} for this kind of AutoModel: {cls.__name__}.\n"
+            f"Model type should be one of {', '.join(c.__name__ for c in CRF_MODEL_MAPPINGS.keys())}."
+        )
+
+    @classmethod
+    def from_pretrained(cls, model_name_or_path, *model_args, **kwargs):
+        config = kwargs.pop("config", None)
+        trust_remote_code = kwargs.pop("trust_remote_code", False)
+        if not isinstance(config, PretrainedConfig):
+            config, kwargs = AutoConfig.from_pretrained(
+                model_name_or_path,
+                return_unused_kwargs=True,
+                **kwargs,
+            )
+        if type(config) in cls._model_mapping.keys():
+            model_class = CRF_MODEL_MAPPINGS[type(config)]
+            return model_class.from_pretrained(
+                model_name_or_path, *model_args, config=config, **kwargs
+            )
+        raise ValueError(
+            f"Unrecognized configuration class {config.__class__} for this kind of AutoModel: {cls.__name__}.\n"
+            f"Model type should be one of {', '.join(c.__name__ for c in CRF_MODEL_MAPPINGS.keys())}."
+        )
