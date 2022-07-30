5bd5b72a5f6cbc6d82ce4b1d0943c3e908d18fe4
Amardeep <kumaramardipsingh@gmail.com>
2022-07-30 14:28:56 +0530

    formating nits

--- src/transformerkp/tagger/data_collators.py
+++ src/transformerkp/tagger/data_collators.py
@@ -5,4 +5,5 @@ class DataCollatorForKpExtraction(DataCollatorForTokenClassification):
     """
     A Data collator class for keyphrase extraction built on the top of HF DataCollatorForTokenClassification collator.
     """
+
     pass
